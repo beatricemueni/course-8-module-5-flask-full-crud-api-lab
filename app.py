3348b003643a4598d8fe37ecd8bb6eb020f6cee1
beatricemueni <muenisophie8@gmail.com>
2026-06-28 19:53:24 +0300

    Add POST,PATCH and DELETE routes for events

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request       
 
 app = Flask(__name__)
 
@@ -22,33 +22,65 @@ events = [
 @app.route("/events", methods=["POST"])
 def create_event():
     # TODO: Task 2 - Design and Develop the Code
-
     # TODO: Task 3 - Implement the Loop and Process Each Element
-
     # TODO: Task 4 - Return and Handle Results
-    pass
+
+    data = request.get_json()
+
+    if not data or "title" not in data:
+        return jsonify({"error": "Title is required"}), 400
+
+    # Create new ID
+    new_id = len(events) + 1
+
+    new_event = Event(new_id, data["title"])
+
+    events.append(new_event)
+
+    return jsonify(new_event.to_dict()), 201
+    
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
     # TODO: Task 2 - Design and Develop the Code
-
     # TODO: Task 3 - Implement the Loop and Process Each Element
-
     # TODO: Task 4 - Return and Handle Results
-    pass
+
+
+    data = request.get_json()
+
+    for event in events:
+        if event.id == event_id:
+
+            if "title" in data:
+                event.title = data["title"]
+
+            return jsonify(event.to_dict()), 200
+
+    return jsonify({"error": "Event not found"}), 404
+    
 
 # TODO: Task 1 - Define the Problem
 # Remove an event from the list
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
     # TODO: Task 2 - Design and Develop the Code
-
     # TODO: Task 3 - Implement the Loop and Process Each Element
-
     # TODO: Task 4 - Return and Handle Results
-    pass
+
+    for event in events:
+        if event.id == event_id:
+
+            events.remove(event)
+
+            return jsonify({
+                "message": "Event deleted successfully"
+            }), 200
+
+    return jsonify({"error": "Event not found"}), 404
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
